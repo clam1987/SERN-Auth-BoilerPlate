b9931a02bf6c13c1b3b39c7c1b562ad5330b6bbb
Wilson Lam <lin-1987@live.com>
2019-12-09 23:45:21 -0800

    Initialized and Connected to Server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,38 @@
+// Modules
 const express = require("express");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+// Global Variables
+const PORT = process.env.PORT || 3001;
+
+// Initalize express
 const app = express();
-const PORT = process.env.PORT || 5001
+
+// Initalize dotenv
+dotenv.config();
+
+// Body Parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Routes
+
+
+
+// Connection to server
+app.listen(PORT, () => {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+      );
+});
+
